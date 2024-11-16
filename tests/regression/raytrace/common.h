60181b6541f9d5dc2b1f7c2fd46af935b5de23f2
Bhavik <bhavik.joshi28@icloud.com>
2024-11-16 04:05:48 +0000

    Add initial main.cpp for RT test (untested)

--- tests/regression/raytrace/common.h
+++ tests/regression/raytrace/common.h
@@ -5,18 +5,57 @@
 
 #define KERNEL_ARG_DEV_MEM_ADDR 0x7ffff000
 
+#define NUM_TRIANGLES 64
+
+struct float3 {
+  float x, y, z;
+  float3 operator-(const float3& a) const {
+    return {x - a.x, y - a.y, z - a.z};
+  }
+  float3 operator+(const float3& a) const {
+    return {x + a.x, y + a.y, z + a.z};
+  }
+  float3 operator*(const float& s) const {
+    return {x * s, y * s, z * s};
+  }
+  float3& operator=(const float3& a) {
+    x = a.x;
+    y = a.y;
+    z = a.z;
+    return *this;
+  }
+};
+
+struct Tri { 
+  float3 vertex0, vertex1, vertex2; 
+  float3 centroid; 
+};
+
+struct Ray {
+  float3 O, D;
+  float t = 1e30f;
+};
+
+struct BVHNode {
+    float3 aabbMin, aabbMax;
+    uint leftNode, firstTriIdx, triCount;
+    bool isLeaf() { return triCount > 0; }
+};
+
 typedef struct {
-  uint32_t num_tasks;
+  uint32_t grid_dim[2];
+  uint32_t num_triangles;
 
   uint32_t dst_width;
   uint32_t dst_height;
-
-  uint64_t cbuf_addr;  
+ 
   uint8_t  cbuf_stride;  
   uint32_t cbuf_pitch;  
 
-  uint64_t prim_addr;
-  bool     use_sw;
+  uint64_t bvh_addr;
+  uint64_t tri_addr;
+  uint64_t triIdx_addr;
+  uint64_t cbuf_addr; 
 } kernel_arg_t;
 
 #endif
--- tests/regression/raytrace/main.cpp
+++ tests/regression/raytrace/main.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
-#include <vector>
 #include <unistd.h>
-#include <cstring>
+#include <string.h>
+#include <vector>
 #include <chrono>
-#include <cmath>
-#include <array>
-#include <assert.h>
 #include <vortex.h>
-#include <graphics.h>
-#include <gfxutil.h>
-#include <VX_config.h>
-#include <algorithm>
+#include <cmath>
+#include <math.h>
 #include "common.h"
-#include <cocogfx/include/imageutil.hpp>
 
-using namespace cocogfx;
-
-#ifndef ASSETS_PATHS
-#define ASSETS_PATHS ""
-#endif
+#define FLOAT_ULP 6
 
 #define RT_CHECK(_expr)                                         \
    do {                                                         \
@@ -32,67 +22,140 @@ using namespace cocogfx;
 
 ///////////////////////////////////////////////////////////////////////////////
 
+static void generate_triangles(Tri[] tri, int N) {
+    for (int i = 0; i < N; i++)
+    {
+        float3 r0{static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX};
+        float3 r1{static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX};
+        float3 r2{static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX, static_cast<float>(rand()) / RAND_MAX};
+        tri[i].vertex0 = r0 * 9 - float3{5, 5, 5};
+        tri[i].vertex1 = tri[i].vertex0 + r1;
+        tri[i].vertex2 = tri[i].vertex0 + r2;
+    }
+}
+
+static void UpdateNodeBounds(BVHNode[] bvhNode, Tri[] tri, uint[] triIdx, int N, uint nodeIdx)  {
+    BVHNode& node = bvhNode[nodeIdx];
+    node.aabbMin = float3( 1e30f , 1e30f , 1e30f );
+    node.aabbMax = float3( -1e30f, -1e30f, -1e30f );
+    for (uint first = node.firstTriIdx, i = 0; i < node.triCount; i++)
+    {
+        uint leafTriIdx = triIdx[first + i];
+        Tri& leafTri = tri[leafTriIdx];
+        node.aabbMin = fminf( node.aabbMin, leafTri.vertex0 ),
+        node.aabbMin = fminf( node.aabbMin, leafTri.vertex1 ),
+        node.aabbMin = fminf( node.aabbMin, leafTri.vertex2 ),
+        node.aabbMax = fmaxf( node.aabbMax, leafTri.vertex0 ),
+        node.aabbMax = fmaxf( node.aabbMax, leafTri.vertex1 ),
+        node.aabbMax = fmaxf( node.aabbMax, leafTri.vertex2 );
+    }
+}
+
+static void Subdivide(BVHNode[] bvhNode, Tri[] tri, uint[] triId, intN, uint nodeIdx )
+{
+  // terminate recursion
+  BVHNode& node = bvhNode[nodeIdx];
+  if (node.triCount <= 2) return;
+  // determine split axis and position
+  float3 extent = node.aabbMax - node.aabbMin;
+  int axis = 0;
+  if (extent.y > extent.x) axis = 1;
+  if (extent.z > extent[axis]) axis = 2;
+  float splitPos = node.aabbMin[axis] + extent[axis] * 0.5f;
+  // in-place partition
+  int i = node.firstTriIdx;
+  int j = i + node.triCount - 1;
+  while (i <= j)
+  {
+    if (tri[triIdx[i]].centroid[axis] < splitPos)
+      i++;
+    else
+      auto temp = triIdx[i];
+      triIdx[i] = triIdx[j];
+      triIdx[j--] = temp;
+  }
+  // abort split if one of the sides is empty
+  int leftCount = i - node.firstTriIdx;
+  if (leftCount == 0 || leftCount == node.triCount) return;
+  // create child nodes
+  int leftChildIdx = nodesUsed++;
+  int rightChildIdx = nodesUsed++;
+  bvhNode[leftChildIdx].firstTriIdx = node.firstTriIdx;
+  bvhNode[leftChildIdx].triCount = leftCount;
+  bvhNode[rightChildIdx].firstTriIdx = i;
+  bvhNode[rightChildIdx].triCount = node.triCount - leftCount;
+  node.leftNode = leftChildIdx;
+  node.triCount = 0;
+  UpdateNodeBounds( leftChildIdx );
+  UpdateNodeBounds( rightChildIdx );
+  // recurse
+  Subdivide( leftChildIdx );
+  Subdivide( rightChildIdx );
+}
+
+
+uint rootNodeIdx = 0, nodesUsed = 1;
+
+static void BuildBVH(BVHNode[] bvhNode, Tri[] tri, int N, uint rootNodeIdx, uint nodesUsed){
+  for (int i = 0; i < N; i++) tri[i].centroid = 
+            (tri[i].vertex0 + tri[i].vertex1 + tri[i].vertex2) * 0.3333f;
+  // assign all triangles to root node
+  BVHNode& root = bvhNode[rootNodeIdx];
+  root.leftChild = root.rightChild = 0;
+  root.firstPrim = 0, root.primCount = N;
+  UpdateNodeBounds( rootNodeIdx );
+  // subdivide recursively
+  Subdivide( rootNodeIdx );
+}
+
 const char* kernel_file = "kernel.vxbin";
-const char* trace_file  = "triangle.cgltrace";
 const char* output_file = "output.png";
-const char* reference_file  = nullptr;
+uint32_t size = 128;
 
 uint32_t clear_color = 0xff000000;
 
-uint32_t dst_width  = 128;
-uint32_t dst_height = 128;
+uint32_t dst_width  = size;
+uint32_t dst_height = size;
 
 uint32_t cbuf_stride;
 uint32_t cbuf_pitch;
 uint32_t cbuf_size;
 
-uint64_t cbuf_addr;
-uint64_t tilebuf_addr;
-uint64_t primbuf_addr;
-
-vx_device_h device      = nullptr;
+vx_device_h device = nullptr;
+vx_buffer_h BVH_buffer = nullptr;
+vx_buffer_h tri_buffer = nullptr;
+vx_buffer_h triIdx_buffer = nullptr;
+vx_buffer_h color_buffer = nullptr;
 vx_buffer_h krnl_buffer = nullptr;
 vx_buffer_h args_buffer = nullptr;
-vx_buffer_h color_buffer= nullptr;
-vx_buffer_h tile_buffer = nullptr;
-vx_buffer_h prim_buffer = nullptr;
-
-bool use_sw = false;
 
 kernel_arg_t kernel_arg = {};
 
-uint32_t tileLogSize = RASTER_TILE_LOGSIZE;
+Tri tri[NUM_TRIANGLES];
+uint triIdx[NUM_TRIANGLES];
+BVHNode bvhNode[NUM_TRIANGLES * 2 - 1];
 
 static void show_usage() {
-   std::cout << "Vortex rasterizer Test." << std::endl;
-   std::cout << "Usage: [-t trace] [-o output] [-r reference] [-w width] [-h height] [-z no_hw] [-k tilelogsize]" << std::endl;
+   std::cout << "Vortex Ray Tracing Test." << std::endl;
+   std::cout << "Usage: [-k: kernel] [-n size] [-h: help]" << std::endl;
 }
 
 static void parse_args(int argc, char **argv) {
   int c;
-  while ((c = getopt(argc, argv, "t:i:o:r:w:h:t:k:z?")) != -1) {
+  while ((c = getopt(argc, argv, "n:o:k:h?")) != -1) {
     switch (c) {
-    case 't':
-      trace_file = optarg;
+    case 'n':
+      size = atoi(optarg);
+      dst_width  = size;
+      dst_height = size;
+      break;
+    case 'k':
+      kernel_file = optarg;
       break;
     case 'o':
       output_file = optarg;
       break;
-    case 'r':
-      reference_file = optarg;
-      break;
-    case 'w':
-      dst_width = std::atoi(optarg);
-      break;
     case 'h':
-      dst_height = std::atoi(optarg);
-      break;
-    case 'k':
-      tileLogSize = std::atoi(optarg);
-      break;
-    case 'z':
-      use_sw = true;
-      break;
     case '?': {
       show_usage();
       exit(0);
@@ -102,90 +165,109 @@ static void parse_args(int argc, char **argv) {
       exit(-1);
     }
   }
-  if (strcmp (output_file, "null") == 0 && reference_file) {
-    std::cout << "Error: the output file is missing for reference validation!" << std::endl;
-    exit(1);
-  }
 }
 
 void cleanup() {
-  vx_mem_free(color_buffer);
-  vx_mem_free(tile_buffer);
-  vx_mem_free(prim_buffer);
-  vx_mem_free(krnl_buffer);
-  vx_mem_free(args_buffer);
-  vx_dev_close(device);
+  if (device) {
+    vx_mem_free(BVH_buffer);
+    vx_mem_free(tri_buffer);
+    vx_mem_free(triIdx_buffer);
+    vx_mem_free(color_buffer);
+    vx_mem_free(krnl_buffer);
+    vx_mem_free(args_buffer);
+    vx_dev_close(device);
+  }
 }
 
-int render(const CGLTrace& trace) {
-  // render each draw call
-  for (auto& drawcall : trace.drawcalls) {
-    std::vector<uint8_t> tilebuf;
-    std::vector<uint8_t> primbuf;
+int main(int argc, char *argv[]) {
+  // parse command arguments
+  parse_args(argc, argv);
 
-    // Perform tile binning
-    auto num_tiles = graphics::Binning(tilebuf, primbuf, drawcall.vertices, drawcall.primitives, dst_width, dst_height, drawcall.viewport.near, drawcall.viewport.far, tileLogSize);
-    std::cout << "Binning allocated " << std::dec << num_tiles << " tiles with " << primbuf.size() << " total primitives." << std::endl;
-    if (0 == num_tiles)
-      continue;
+  std::srand(50);
 
-    // allocate tile memory
-    if (tile_buffer != nullptr) vx_mem_free(tile_buffer);
-    if (prim_buffer != nullptr) vx_mem_free(prim_buffer);
-    RT_CHECK(vx_mem_alloc(device, tilebuf.size(), VX_MEM_READ, &tile_buffer));
-    RT_CHECK(vx_mem_address(tile_buffer, &tilebuf_addr));
-    RT_CHECK(vx_mem_alloc(device, primbuf.size(), VX_MEM_READ, &prim_buffer));
-    RT_CHECK(vx_mem_address(prim_buffer, &primbuf_addr));
-    std::cout << "tile_buffer=0x" << std::hex << tilebuf_addr << std::dec << std::endl;
-    std::cout << "prim_buffer=0x" << std::hex << primbuf_addr << std::dec << std::endl;
+  // open device connection
+  std::cout << "open device connection" << std::endl;
+  RT_CHECK(vx_dev_open(&device));
 
-    // upload tiles buffer
-    std::cout << "upload tile buffer" << std::endl;
-    RT_CHECK(vx_copy_to_dev(tile_buffer, tilebuf.data(), 0, tilebuf.size()));
+  std::cout << "output image size: " << dst_height << "x" << dst_width << std::endl;
 
-    // upload primitives buffer
-    std::cout << "upload primitive buffer" << std::endl;
-    RT_CHECK(vx_copy_to_dev(prim_buffer, primbuf.data(), 0, primbuf.size()));
+  kernel_arg.grid_dim[0] = dst_height;
+  kernel_arg.grid_dim[1] = dst_width;
+  kernel_arg.num_triangles = NUM_TRIANGLES;
 
-    // upload kernel argument
-    std::cout << "upload kernel argument" << std::endl;
+  cbuf_stride = 4;
+  cbuf_pitch  = dst_width * cbuf_stride;
+  cbuf_size   = dst_height * cbuf_pitch;
+
+  int bvh_size = sizeof(BVHNode) * (2 * NUM_TRIANGLES - 1);
+  int tri_size = sizeof(Tri) * NUM_TRIANGLES;
+  int triIdx_size = sizeof(uint) * NUM_TRIANGLES;
+
+  // allocate device memory
+  std::cout << "allocate device memory" << std::endl;
+  // bvh buffer
+  RT_CHECK(vx_mem_alloc(device, bvh_size, VX_MEM_READ, &BVH_buffer));
+  RT_CHECK(vx_mem_address(BVH_buffer, &kernel_arg.bvh_addr));
+  // tri buffer
+  RT_CHECK(vx_mem_alloc(device, tri_size, VX_MEM_READ, &tri_buffer));
+  RT_CHECK(vx_mem_address(tri_buffer, &kernel_arg.tri_addr));
+  // triIdx buffer
+  RT_CHECK(vx_mem_alloc(device, triIdx_size, VX_MEM_READ, &triIdx_buffer));
+  RT_CHECK(vx_mem_address(triIdx_buffer, &kernel_arg.triIdx_addr));
+  // color buffer
+  RT_CHECK(vx_mem_alloc(device, cbuf_size, VX_MEM_WRITE, &color_buffer));
+  RT_CHECK(vx_mem_address(color_buffer, &kernel_arg.cbuf_addr));
+
+  std::cout << "bvh_addr=0x"    << std::hex << BVH_buffer    << std::endl;
+  std::cout << "tri_addr=0x"    << std::hex << tri_buffer    << std::endl;
+  std::cout << "triIdx_addr=0x" << std::hex << triIdx_buffer << std::endl;
+  std::cout << "cbuf_addr=0x"   << std::hex << color_buffer  << std::endl;
+
+  // upload bvh buffer
+  {
+    std::cout << "upload bvh buffer" << std::endl;
+    RT_CHECK(vx_copy_to_dev(BVH_buffer, bvhNode, 0, bvh_size));
+  }
+
+ // upload tri buffer
+  {
+    std::cout << "upload tri buffer" << std::endl;
+    RT_CHECK(vx_copy_to_dev(tri_addr, tri, 0, buf_size));
+  }
+
+  // upload triIdx buffer
+  {
+    std::cout << "upload triIdx buffer" << std::endl;
+    RT_CHECK(vx_copy_to_dev(triIdx_addr, triIdx, 0, buf_size));
+  }
+
+  // upload program
+  std::cout << "upload program" << std::endl;
+  RT_CHECK(vx_upload_kernel_file(device, kernel_file, &krnl_buffer));
+
+  // upload kernel argument
+  std::cout << "upload kernel argument" << std::endl;
     {
-      kernel_arg.use_sw      = use_sw;
-      kernel_arg.prim_addr   = primbuf_addr;
       kernel_arg.dst_width   = dst_width;
       kernel_arg.dst_height  = dst_height;
-      kernel_arg.cbuf_addr   = cbuf_addr;
       kernel_arg.cbuf_stride = cbuf_stride;
       kernel_arg.cbuf_pitch  = cbuf_pitch;
-
-      RT_CHECK(vx_upload_bytes(device, &kernel_arg, sizeof(kernel_arg_t), &args_buffer));
     }
+  RT_CHECK(vx_upload_bytes(device, &kernel_arg, sizeof(kernel_arg_t), &args_buffer));
 
-    uint32_t primbuf_stride = sizeof(graphics::rast_prim_t);
+  auto time_start = std::chrono::high_resolution_clock::now();
 
-    // configure raster units
-    vx_dcr_write(device, VX_DCR_RASTER_TBUF_ADDR, tilebuf_addr / 64);  // block address
-    vx_dcr_write(device, VX_DCR_RASTER_TILE_COUNT, num_tiles);
-    vx_dcr_write(device, VX_DCR_RASTER_PBUF_ADDR, primbuf_addr / 64);  // block address
-    vx_dcr_write(device, VX_DCR_RASTER_PBUF_STRIDE, primbuf_stride);
-    vx_dcr_write(device, VX_DCR_RASTER_SCISSOR_X, (dst_width << 16) | 0);
-    vx_dcr_write(device, VX_DCR_RASTER_SCISSOR_Y, (dst_height << 16) | 0);
+  // start device
+  std::cout << "start device" << std::endl;
+  RT_CHECK(vx_start(device, krnl_buffer, args_buffer));
 
-    auto time_start = std::chrono::high_resolution_clock::now();
+  // wait for completion
+  std::cout << "wait for completion" << std::endl;
+  RT_CHECK(vx_ready_wait(device, VX_MAX_TIMEOUT));
 
-    // start device
-    std::cout << "start device" << std::endl;
-    RT_CHECK(vx_start(device, krnl_buffer, args_buffer));
-
-    // wait for completion
-    std::cout << "wait for completion" << std::endl;
-    RT_CHECK(vx_ready_wait(device, VX_MAX_TIMEOUT));
-
-    auto time_end = std::chrono::high_resolution_clock::now();
-    double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
-    printf("Elapsed time: %lg ms\n", elapsed);
-
-  }
+  auto time_end = std::chrono::high_resolution_clock::now();
+  double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
+  printf("Elapsed time: %lg ms\n", elapsed);
 
   // save output image
   if (strcmp(output_file, "null") != 0) {
@@ -197,86 +279,21 @@ int render(const CGLTrace& trace) {
     RT_CHECK(SaveImage(output_file, FORMAT_A8R8G8B8, bits, dst_width, dst_height, -cbuf_pitch));
   }
 
-  return 0;
-}
-
-int main(int argc, char *argv[]) {
-  // parse command arguments
-  parse_args(argc, argv);
-
-  // open device connection
-  std::cout << "open device connection" << std::endl;
-  RT_CHECK(vx_dev_open(&device));
-
-  uint64_t isa_flags;
-  RT_CHECK(vx_dev_caps(device, VX_CAPS_ISA_FLAGS, &isa_flags));
-  bool has_ext = (isa_flags & VX_ISA_EXT_RASTER) != 0;
-  if (!has_ext) {
-    std::cout << "RASTER extensions not supported!" << std::endl;
-    cleanup();
-    return -1;
-  }
-
-  uint64_t num_cores, num_warps, num_threads;
-  RT_CHECK(vx_dev_caps(device, VX_CAPS_NUM_CORES, &num_cores));
-  RT_CHECK(vx_dev_caps(device, VX_CAPS_NUM_WARPS, &num_warps));
-  RT_CHECK(vx_dev_caps(device, VX_CAPS_NUM_THREADS, &num_threads));
-
-  uint32_t num_tasks = num_cores * num_warps * num_threads;
-
-  std::cout << "number of tasks: " << std::dec << num_tasks << std::endl;
-
-  CGLTrace trace;
-  auto trace_file_s = graphics::ResolveFilePath(trace_file, ASSETS_PATHS);
-  RT_CHECK(trace.load(trace_file_s.c_str()));
-
-  // upload program
-  std::cout << "upload program" << std::endl;
-  RT_CHECK(vx_upload_kernel_file(device, kernel_file, &krnl_buffer));
-
-  cbuf_stride = 4;
-  cbuf_pitch  = dst_width * cbuf_stride;
-  cbuf_size   = dst_height * cbuf_pitch;
-
-  // allocate device memory
-  RT_CHECK(vx_mem_alloc(device, cbuf_size, VX_MEM_WRITE, &color_buffer));
-  RT_CHECK(vx_mem_address(color_buffer, &cbuf_addr));
-  std::cout << "color_buffer=0x" << std::hex << cbuf_addr << std::dec << std::endl;
-
-  // clear destination buffer
-  std::cout << "clear destination buffer" << std::endl;
-  {
-    std::vector<uint32_t> staging_buf(cbuf_size / 4, clear_color);
-    RT_CHECK(vx_copy_to_dev(color_buffer, staging_buf.data(), 0, cbuf_size));
-  }
-
-  // update kernel arguments
-  kernel_arg.num_tasks  = num_tasks;
-  kernel_arg.dst_width  = dst_width;
-  kernel_arg.dst_height = dst_height;
-
-  kernel_arg.cbuf_stride= cbuf_stride;
-  kernel_arg.cbuf_pitch = cbuf_pitch;
-  kernel_arg.cbuf_addr  = cbuf_addr;
-
-  // run tests
-  std::cout << "render" << std::endl;
-  RT_CHECK(render(trace));
+  // verify result
+  std::cout << "check image for result, no verification code yet" << std::endl;
+  // TODO: add verification code
 
   // cleanup
   std::cout << "cleanup" << std::endl;
   cleanup();
 
-  if (strcmp (output_file, "") != 0 && reference_file) {
-    auto reference_file_s = graphics::ResolveFilePath(reference_file, ASSETS_PATHS);
-    auto errors = CompareImages(output_file, reference_file_s.c_str(), FORMAT_A8R8G8B8);
-    if (0 == errors) {
-      std::cout << "PASSED!" << std::endl;
-    } else {
-      std::cout << "FAILED! " << errors << " errors." << std::endl;
-      return errors;
-    }
-  }
+  /*if (errors != 0) {
+    std::cout << "Found " << std::dec << errors << " errors!" << std::endl;
+    std::cout << "FAILED!" << std::endl;
+    return errors;
+  }*/
+
+  std::cout << "PASSED!" << std::endl;
 
   return 0;
 }
